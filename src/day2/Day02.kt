753984163bb2761ec50108ac1162547d01bf87e2
bstein <stone.bastian@gmail.com>
2023-12-02 15:24:33 +0100

    d2p2

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -9,6 +9,7 @@ fun main() {
     val input = readInput("day2/Day02")
 
     println("Day 02, Part 1: ${part1(input, CubeComposition(12, 13, 14))}")
+    println("Day 02, Part 2: ${part2(input)}")
 }
 
 
@@ -30,5 +31,20 @@ fun part1(input: List<String>, referenceComposition: CubeComposition): Int {
             true -> lineID.toString().toInt()
             false -> 0
         }
+    }
+}
+
+fun part2(input: List<String>): Int {
+    return input.sumOf { it ->
+        val parts = it.split(":")
+
+        val cubeLines = parts.last().split(";")
+        val cubeComps = cubeLines.map { cubeLine -> CubeReader.readCubes(cubeLine) }
+
+        val minRedCubes = cubeComps.maxOf { it.red }
+        val minGreenCubes = cubeComps.maxOf { it.green }
+        val minBlueCubes = cubeComps.maxOf { it.blue }
+
+        minRedCubes * minGreenCubes * minBlueCubes
     }
 }
