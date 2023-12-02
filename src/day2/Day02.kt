665ae03603870481787c2e53a383a1eb121dc8b7
bstein <stone.bastian@gmail.com>
2023-12-02 15:17:43 +0100

    tidy 2-part1

--- src/day2/Day02.kt
+++ src/day2/Day02.kt
@@ -6,22 +6,29 @@ import readInput
 data class CubeComposition(val red: Int, val green: Int, val blue: Int)
 
 fun main() {
-    val input = readInput("day2/Day02.txt2.txt")
-    println("Day 02, Part 1: ${day1.part1(input)}")
+    val input = readInput("day2/Day02")
 
+    println("Day 02, Part 1: ${part1(input, CubeComposition(12, 13, 14))}")
 }
 
 
-fun part1(input: List<String>, composition: CubeComposition): Int {
+fun part1(input: List<String>, referenceComposition: CubeComposition): Int {
     return input.sumOf {
         val parts = it.split(":")
         val lineID = parts.first().substringAfter("Game ").toInt()
 
-        val cubes = parts.last().split(",").map { it.trim() }
-        println(cubes)
+        val cubeLines = parts.last().split(";")
+        val cubeComps = cubeLines.map { cubeLine -> CubeReader.readCubes(cubeLine) }
+        val satisfiedReferenceComp = cubeComps.all { cubes ->
+            val validRed = cubes.red <= referenceComposition.red
+            val validGreen = cubes.green <= referenceComposition.green
+            val validBlue = cubes.blue <= referenceComposition.blue
+            validRed && validGreen && validBlue
+        }
 
-
-
-        lineID.toString().toInt()
+        when (satisfiedReferenceComp) {
+            true -> lineID.toString().toInt()
+            false -> 0
+        }
     }
 }
